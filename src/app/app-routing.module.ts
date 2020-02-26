284835106e64b13e68d7d6c99a6b8a8fb2f17ba9
Brandon Sanchez <bssanchez93@gmail.com>
2020-02-26 13:01:52 -0500

    [*] Created routing struct and principal components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'post', pathMatch: 'full' },
+  { path: 'post', loadChildren: () => import('./pages/post/post.module').then(m => m.PostModule) },
+  { path: 'error', loadChildren: () => import('./pages/errors/errors.module').then(m => m.ErrorsModule) },
+  { path: '**', redirectTo: '/error/not-found', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/post/post-routing.module.ts
+++ src/app/pages/post/post-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ListComponent } from './list/list.component';
+import { DetailComponent } from './detail/detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'list', pathMatch: 'full' },
+  { path: 'list', component: ListComponent },
+  { path: 'detail/{id}', component: DetailComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
